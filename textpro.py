b299ddf01510e37048d12d8b04152de5f0cc1dea
Matheus Matias <matheusomr1@outlook.com>
2020-12-31 13:44:49 -0300

    Error regarding the capitalization of the strings fixed

--- textpro.py
+++ textpro.py
@@ -1,8 +1,8 @@
 interrogatives = ['who', 'whom', 'which', 'whose', 'where', 'when', 'why', 'what', 'how', 'have']
 
-def title_str(str_array):                       #titles all strings on the array
-    for i in range(0,len(str_array)):
-        str_array[i] = str_array[i].title()
+def capitalize_str(str_array):                       #capitalizes all strings on the array
+    for i in range(0, len(str_array)):
+        str_array[i] = str_array[i].capitalize()
     return str_array
 
 
@@ -11,6 +11,7 @@ def ver_if_question(string):                    #verifies if the current string 
         if string.startswith(each) == True:
             string = string + '?'
             return string
+
     return f'{string}.'
 
 str_input = ''
@@ -24,4 +25,4 @@ while True:
         str_array.append(ver_if_question(str_input))
         continue
 
-print(title_str(str_array))
+print(capitalize_str(str_array))
